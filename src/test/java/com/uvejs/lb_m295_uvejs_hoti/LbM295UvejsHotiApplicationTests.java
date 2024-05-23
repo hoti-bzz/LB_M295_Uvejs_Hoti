728a6dd5ba3b06f7dbb56232578fa2988fe1bf1f
uvejs <hotiu@bzz.ch>
2024-05-23 08:55:12 +0200

    -commit

---
+++ src/test/java/com/uvejs/lb_m295_uvejs_hoti/LbM295UvejsHotiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvejs.lb_m295_uvejs_hoti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbM295UvejsHotiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
